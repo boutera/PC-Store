a5cb7b48e576f009527f3a874682981239d8a68b
boutera <57544621+boutera@users.noreply.github.com>
2021-05-07 23:49:54 +0000

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-	$con=mysqli_connect("sql203.epizy.com","epiz_28369822","x8PaRLs98pN36","epiz_28369822_ecommerce");
+	$con=mysqli_connect("localhost","root","","ecommerce2");
 
 if (mysqli_connect_errno())
 {
